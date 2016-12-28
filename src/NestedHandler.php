fd377d490e34cb8a04ecdda89362da1239fb83d5
Anderson D. Rosa <andersondrosa@outlook.com>
2016-12-28 20:04:35 -0200

    commit

---
+++ src/NestedHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace NestedHandler;
+
+class NestedHandler
+{
+    use \Asterid\Traits\Listener;
+
+    public function __construct(array $config = null)
+    {
+
+    }
+
+    public function render($content)
+    {
+        $p = "|\{([^{}]*)\}|U";
+
+        $self = $this;
+
+        for ($i = 0; $i < 10000; $i++) {
+
+            $success = false;
+
+            $content = preg_replace_callback($p, function ($m) use ($self, &$success) {
+
+                $success = 1;
+
+                $bag = new \stdClass;
+                $bag->content = $m[1] == ' ' ? null : $m[1];
+
+                $self->fire('each', [$bag]);
+
+                return $bag->content;
+
+            }, $content);
+
+            if (!$success) {
+                break;
+            }
+        }
+
+        return $content;
+    }
+
+}
